f0814bb74839935e506daec41a9138c2398c03a0
Michael Hatmaker <mhatmaker@gmail.com>
2019-06-29 00:19:37 -0500

    added ZeroMQ project(s)

---
+++ ZeroMQ/ZeroMQ/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZMQ;
+
+namespace ZeroMqClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                /*using (var socket = context.Socket(SocketType.REQ))
+                {
+                    socket.Connect("tcp://127.0.0.1:5000");
+
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        var reqMsg = "this is a request";
+                        Console.WriteLine("CLIENT Sending: " + reqMsg);
+                        socket.Send(reqMsg, Encoding.UTF8);
+                        var replyMsg = socket.Recv(Encoding.UTF8);
+                        Console.WriteLine("CLIENT Received: " + replyMsg);
+                    }
+                } // socket*/
+
+                using (var socket = context.Socket(SocketType.PUB))
+                {
+                    socket.Bind("tcp://127.0.0.1:5000");
+
+                    int messagedata = 88;
+
+                    while (true)
+                    {                        
+                        Thread.Sleep(1000);  
+
+                        var topic = "russia";
+                        ++messagedata;
+                        var sendMsg = string.Format("{0} {1}", topic, messagedata);
+                        Console.WriteLine("publishing: " + sendMsg);
+                        socket.Send(sendMsg, Encoding.UTF8);
+
+                        topic = "turkey";
+                        ++messagedata;
+                        sendMsg = string.Format("{0} {1}", topic, messagedata);
+                        Console.WriteLine("publishing: " + sendMsg);
+                        socket.Send(sendMsg, Encoding.UTF8);
+
+                    }
+                } // socket
+
+            } // context
+        }
+
+    } // CLASS
+} // NAMESPACE
---
+++ ZeroMQ/ZeroMqServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZMQ;
+
+namespace ZeroMqServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                /*using (var socket = context.Socket(SocketType.REP))
+                {
+                    socket.Bind("tcp://127.0.0.1:5000");
+
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        var rcvdMsg = socket.Recv(Encoding.UTF8);
+                        Console.WriteLine("SERVER Received: " + rcvdMsg);
+                        var replyMsg = "This is your reply!";
+                        Console.WriteLine("SERVER Sending: " + replyMsg);
+                        socket.Send(replyMsg, Encoding.UTF8);
+                    }
+                } // socket*/
+
+                using (var socket = context.Socket(SocketType.SUB))
+                {
+                    //socket.Subscribe(Encoding.UTF8.GetBytes("russia"));
+                    socket.Subscribe("", Encoding.UTF8);
+                    socket.Connect("tcp://127.0.0.1:5000");
+                    
+/*# Subscribe to zipcode, default is NYC, 10001
+topicfilter = "10001"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)*/
+
+                    while (true)
+                    {
+                        //Thread.Sleep(1000);
+                        Console.WriteLine("Waiting for SUB receive...");
+                        var rcvdMsg = socket.Recv(Encoding.UTF8);
+                        Console.WriteLine("SUBSCRIBED DATA RECEIVED: {0}", rcvdMsg);
+                        //string[] split = rcvdMsg.Split();
+                        //_topic = split[0];
+                        //_data = split[1];
+                    }
+                }
+
+            } // context
+        }
+
+    } // CLASS
+} // NAMESPACE
